805310cd76c5300f04c3c7ca3baaeacd62c41429
joyboy18900 <surakiat.dev@gmail.com>
2023-07-27 14:35:43 +0700

    add test case about Injectmocks

--- day1/src/test/java/com/example/day1/users/UserCommandServiceTest.java
+++ day1/src/test/java/com/example/day1/users/UserCommandServiceTest.java
@@ -2,6 +2,7 @@ package com.example.day1.users;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -46,7 +47,7 @@ class UserCommandServiceTest {
     }
 
     @Test
-    void createUser_failure_duplicate_firstname_with_spy(){
+    void createUser_failure_duplicate_firstname_with_spy_v1(){
         UserRequest userRequest = new UserRequest();
         userRequest.setFirstName("Surakiat");
         userRequest.setLastName("Sangkla");
@@ -58,6 +59,26 @@ class UserCommandServiceTest {
         UserCommandService service = new UserCommandService(userRepository);
         try {
             service.createUser(userRequest);
+        } catch (Exception e) {}
+        // Assert
+        verify(userRepository, times(1)).findByFirstName("Surakiat");
+    }
+
+    @InjectMocks
+    UserCommandService userCommandServiceV2;
+
+    @Test
+    void createUser_failure_duplicate_firstname_with_spy_v2(){
+        UserRequest userRequest = new UserRequest();
+        userRequest.setFirstName("Surakiat");
+        userRequest.setLastName("Sangkla");
+
+        List<UserEntity> results = new ArrayList<>();
+        results.add(new UserEntity());
+        when(userRepository.findByFirstName("Surakiat")).thenReturn(results);
+
+        try {
+            userCommandServiceV2.createUser(userRequest);
         } catch (Exception e) {}
         // Assert
         verify(userRepository, times(1)).findByFirstName("Surakiat");
